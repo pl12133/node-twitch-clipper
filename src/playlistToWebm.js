cbc3b339553dd980aa46bc06b674c7dc4b5ebbee
pl12133 <krirken@gmail.com>
2016-02-20 17:47:33 -0500

    Automatically add webm extension if it is missing

--- src/playlistToWebm.js
+++ src/playlistToWebm.js
@@ -2,38 +2,16 @@
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 
-module.exports = {
-  promisePlaylistToWebm: promisePlaylistToWebm
-}
-
-// function playlistToWebm(filename, startTime, duration, stream) {
-//   startTime = startTime || 0;
-//   duration = duration || 10;
-// 
-//   console.log('Trying to convert playlist to ' + filename);
-// 
-//   var command = ffmpeg(stream);
-//   command
-//     .seekInput(startTime)
-//     .videoCodec('libvpx')
-//     .duration(duration)
-//     .on('error', function(err) {
-//       console.error('FFmpeg Error: ', err);
-//     })
-//     .on('end', function() {
-//       console.log(filename + ' saving finished');
-//     })
-//     .on('progress', function(progress) {
-//       console.log('Processing: ' + progress.percent + '% done');
-//     })
-//     .save(filename);
-// }
-
-function promisePlaylistToWebm(filename, startTime, duration, stream) {
+module.exports = function playlistToWebm(filename, startTime, duration, stream) {
   return new Promise(function(resolve, reject) {
     startTime = startTime || 0;
     duration = duration || 10;
-    filename = path.join('videos', path.basename(filename));
+    let basename = path.basename(filename);
+    basename = (basename.lastIndexOf('.webm') < basename.length - '.webm'.length)
+      ? basename += '.webm'
+      : basename;
+
+    filename = path.join('videos', basename);
 
     console.log('Trying to convert playlist to ' + filename);
 
